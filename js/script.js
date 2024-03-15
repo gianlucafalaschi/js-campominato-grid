7134fae5f7a646ce8ab39d8505c48ef329ac59c9
gianlucafalaschi <gian100gian@outlook.it>
2024-03-15 18:22:40 +0100

    feat: function that creates a square with a number

--- js/script.js
+++ js/script.js
@@ -5,13 +5,17 @@ Quando l'utente clicca su ogni cella, la cella cliccata si colora di
 azzurro ed emetto un messaggio in console con il numero della cella cliccata. */
 
 // quando l'utente preme il pulsante play si genera una griglia 
-const playButton = document.querySelector('#play-button');
+/* const playButton = document.querySelector('#play-button');
 console.log(playButton);
 playButton.addEventListener('click', function() {
     // la griglia diventa visibile aggiungendo una classe
     const mainGrid = document.querySelector('#grid');
     mainGrid.classList.toggle('active');
-});
+
+
+}); */
+
+const nuovoQuadrato = generateSquare(1);
 
 //per 100 volte creare un quadratino e ogni quadratino si aggiunge alla 
 // griglia grid
@@ -19,3 +23,17 @@ playButton.addEventListener('click', function() {
 // Quando il quadratino viene cliccato si colora di azzurro e
 // viene emesso un messaggio in console con il numero del quadratino cliccato
 
+
+
+// FUNCTIONS
+
+// Funzione che genera un quadrato
+// number -> numero che rappresenta un numero
+// return: elemento del DOM che rappresenta un quadrato
+
+function generateSquare(number) {
+    const newSquare = document.createElement('div');
+    newSquare.classList.add('square');
+    newSquare.innerHTML = `<span>${number}</span>`;
+    console.log(newSquare)
+}
